c5daabf3000c8663df3cca78e603fb0ee8586e65
yangjun <290968698@qq.com>
2019-04-04 14:38:59 +0800

    impl grpc all api test

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -7,9 +7,11 @@ import (
 	"log"
 	"net"
 	"strconv"
+	"time"
 
 	"grpctest/demo"
 
+	"github.com/golang/glog"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
@@ -20,7 +22,7 @@ type demoServer struct {
 // 单次调用
 func (s *demoServer) SayHello(ctx context.Context, req *demo.Request) (*demo.Reply, error) {
 	md, _ := metadata.FromIncomingContext(ctx)
-	log.Println("SayHello got call:", req, " meta:", md)
+	glog.Info("SayHello got call:", req, " meta:", md)
 
 	reply := &demo.Reply{
 		Message: req.Name,
@@ -32,23 +34,23 @@ func (s *demoServer) SayHello(ctx context.Context, req *demo.Request) (*demo.Rep
 // 客户端流
 func (s *demoServer) SayHelloCliStream(cliStream demo.Demo_SayHelloCliStreamServer) error {
 	md, _ := metadata.FromIncomingContext(cliStream.Context())
-	log.Println("SayHelloCliStream got call meta:", md)
+	glog.Info("SayHelloCliStream got call meta:", md)
 
 	for {
 		req, err := cliStream.Recv()
 
 		if err == io.EOF {
-			log.Println("SayHelloCliStream end")
+			glog.Info("SayHelloCliStream end")
 
 			reply := &demo.Reply{
-				Message: req.Name,
+				Message: "server SayHelloCliStream end",
 			}
 
 			return cliStream.SendAndClose(reply)
 
 		}
 
-		log.Println("SayHelloCliStream got:", req)
+		glog.Info("SayHelloCliStream got:", req)
 	}
 
 	return nil
@@ -57,7 +59,7 @@ func (s *demoServer) SayHelloCliStream(cliStream demo.Demo_SayHelloCliStreamServ
 // 服务端流
 func (s *demoServer) SayHelloServStream(req *demo.Request, stream demo.Demo_SayHelloServStreamServer) error {
 	md, _ := metadata.FromIncomingContext(stream.Context())
-	log.Println("SayHelloServStream got call meta:", md)
+	glog.Info("SayHelloServStream got call meta:", md)
 
 	reqCount, err := strconv.Atoi(req.Name)
 	if err != nil {
@@ -78,8 +80,8 @@ func (s *demoServer) SayHelloServStream(req *demo.Request, stream demo.Demo_SayH
 // 双向流
 func (s *demoServer) SayHelloBidiStream(bistream demo.Demo_SayHelloBidiStreamServer) error {
 	md, _ := metadata.FromIncomingContext(bistream.Context())
-	log.Println("SayHelloBidiStream got call meta:", md)
-
+	glog.Info("SayHelloBidiStream got call meta:", md)
+	replyCount := 0
 	for {
 		req, err := bistream.Recv()
 
@@ -91,8 +93,9 @@ func (s *demoServer) SayHelloBidiStream(bistream demo.Demo_SayHelloBidiStreamSer
 		}
 
 		reply := &demo.Reply{
-			Message: req.Name,
+			Message: req.Name + "reply:" + strconv.Itoa(replyCount),
 		}
+		replyCount++
 		bistream.Send(reply)
 	}
 }
@@ -108,7 +111,141 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	log.Println("Listening on tcp://localhost:6000")
+	glog.Info("Listening on tcp://localhost:6000")
+
+	go grpcClientTest()
+
 	grpcServer.Serve(l)
 
+}
+
+func grpcClientTest() {
+	time.Sleep(time.Second)
+	conn, err := grpc.Dial("localhost:6000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to connect: %s", err)
+	}
+	defer conn.Close()
+
+	client := demo.NewDemoClient(conn)
+
+	go func() {
+		//单次调用
+		req := &demo.Request{
+			Name: "say hello test 1",
+		}
+		reply, err := client.SayHello(context.Background(), req)
+		if err != nil {
+			glog.Info("cli SayHello call err:", err)
+		} else {
+			glog.Info("cli SayHello result:", reply)
+		}
+
+		req.Name = "sya hello test 2"
+		reply, err = client.SayHello(context.Background(), req)
+		if err != nil {
+			glog.Info("cli SayHello call err:", err)
+		} else {
+			glog.Info("cli SayHello result:", reply)
+		}
+
+	}()
+
+	go func() {
+		// 客户端流
+		req := &demo.Request{
+			Name: "cli stream",
+		}
+		cliStream, err := client.SayHelloCliStream(context.Background())
+		if err != nil {
+			glog.Info("cli SayHelloCliStream err:", err)
+			return
+		}
+
+		for i := 0; i < 11; i++ {
+			req.Name = req.Name + strconv.Itoa(i) + ","
+			err = cliStream.Send(req)
+			if err != nil {
+				glog.Info("cli stream send err:", err)
+			}
+		}
+		reply, err := cliStream.CloseAndRecv()
+		if err != nil {
+			glog.Info("cli CloseAndRecv err:", err)
+			return
+		}
+
+		glog.Info("cli CloseAndRecv result:", reply)
+	}()
+
+	go func() {
+		// 服务端流
+		req := &demo.Request{
+			Name: "22",
+		}
+		serStream, err := client.SayHelloServStream(context.Background(), req)
+		if err != nil {
+			glog.Info("cli init SayHelloServStream err:", err)
+			return
+		}
+		for {
+			reply, err := serStream.Recv()
+			if err == io.EOF {
+				glog.Info("cli SayHelloServStream end")
+				return
+			}
+			if err != nil {
+				glog.Info("cli SayHelloServStream err:", err)
+			}
+
+			glog.Info("cli SayHelloServStream got:", reply)
+		}
+	}()
+
+	go func() {
+		// 双向流
+		req := &demo.Request{
+			Name: "bi",
+		}
+		biStream, err := client.SayHelloBidiStream(context.Background())
+		if err != nil {
+			glog.Info("cli init SayHelloServStream err:", err)
+			return
+		}
+		for i := 0; i < 8; i++ {
+			req.Name = "bi " + strconv.Itoa(i)
+			err = biStream.Send(req)
+			if err != nil {
+				glog.Info("cli SayHelloBidiStream send err:", err)
+			}
+
+			reply, err := biStream.Recv()
+
+			if err != nil {
+				glog.Info("cli SayHelloBidiStream recv err:", err)
+			}
+			glog.Info("cli SayHelloBidiStream got:", reply)
+		}
+
+		biStream.CloseSend()
+
+		for {
+			reply, err := biStream.Recv()
+			if err == io.EOF {
+				glog.Info("cli SayHelloBidiStream got end")
+				return
+			}
+			if err != nil {
+				glog.Info("cli SayHelloBidiStream recv err:", err)
+				return
+			}
+
+			glog.Info("cli SayHelloBidiStream got:", reply)
+
+		}
+	}()
+
+	for {
+		time.Sleep(time.Minute)
+	}
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ replace google.golang.org/grpc => github.com/yangjuncode/grpc-go v1.19.2
 
 require (
 	github.com/gogo/protobuf v1.2.0
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	golang.org/x/net v0.0.0-20190311183353-d8887717615a
 	google.golang.org/grpc v0.0.0-00010101000000-000000000000
 )
