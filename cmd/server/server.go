b9ee7c66f7e516c4e166a26180ed87ecb2c763ae
yangjun <290968698@qq.com>
2019-04-05 17:20:18 +0800

    add grpc forward test

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -21,10 +21,10 @@ type demoServer struct {
 // 单次调用
 func (s *demoServer) SayHello(ctx context.Context, req *demo.Request) (*demo.Reply, error) {
 	md, _ := metadata.FromIncomingContext(ctx)
-	glog.Info("SayHello got call:", req, " meta:", md)
+	glog.Info("ser SayHello got call:", req, " meta:", md)
 
 	reply := &demo.Reply{
-		Message: req.Name,
+		Message: req.Name + ":reply",
 	}
 
 	return reply, nil
@@ -113,6 +113,12 @@ func main() {
 	glog.Info("Listening on tcp://localhost:6000")
 
 	go grpcClientTest()
+
+	go func() {
+		time.Sleep(time.Second * 10)
+		glog.Info("forward test begin")
+		forwardGrpcClientTest()
+	}()
 
 	grpcServer.Serve(l)
 
@@ -246,5 +252,198 @@ func grpcClientTest() {
 
 	for {
 		time.Sleep(time.Minute)
+	}
+}
+
+func forwardGrpcClientTest() {
+	time.Sleep(time.Second)
+	conn, err := grpc.Dial("localhost:6000", grpc.WithInsecure())
+	if err != nil {
+		glog.Fatalf("failed to connect: %s", err)
+	}
+	defer conn.Close()
+
+	go func() {
+		//单次调用
+		req := &demo.Request{
+			Name: "say hello test 1 f",
+		}
+		reply := &demo.Reply{}
+
+		reqB, _ := req.Marshal()
+		replyB, err := conn.InvokeForward(context.Background(), "/demo.demo/SayHello", reqB)
+		if err != nil {
+			glog.Info("clif SayHello call err:", err)
+		} else {
+			err = reply.Unmarshal(replyB)
+			glog.Info("clif SayHello result:", reply, err)
+		}
+
+		req.Name = "sya hello test 2 f"
+		reqB, _ = req.Marshal()
+		replyB, err = conn.InvokeForward(context.Background(), "/demo.demo/SayHello", reqB)
+		if err != nil {
+			glog.Info("clif SayHello call err:", err)
+		} else {
+			err = reply.Unmarshal(replyB)
+			glog.Info("clif SayHello result:", reply, err)
+		}
+
+	}()
+
+	go func() {
+		// 客户端流
+		req := &demo.Request{
+			Name: "cli stream",
+		}
+		reply := &demo.Reply{}
+
+		sreamDesc := &grpc.StreamDesc{
+			//StreamName:    "SayHelloCliStream",
+			ClientStreams: true,
+		}
+
+		streamfwd, err := grpc.NewClientStream(context.Background(), sreamDesc, conn, "/demo.demo/SayHelloCliStream")
+		if err != nil {
+			glog.Info("clif NewClientStream err:", err)
+			return
+		}
+
+		for i := 0; i < 11; i++ {
+			req.Name = req.Name + strconv.Itoa(i) + ","
+			reqB, _ := req.Marshal()
+
+			err = streamfwd.SendMsgForward(reqB)
+			if err != nil {
+				glog.Info("clif SayHelloCliStream sendfwd err:", err)
+			}
+		}
+		err = streamfwd.CloseSend()
+		if err != nil {
+			glog.Info("clif SayHelloCliStream CloseSend err:", err)
+		}
+		replyB, err := streamfwd.RecvMsgForward()
+		if err != nil {
+			glog.Info("clif SayHelloCliStream RecvMsgForward err:", err)
+			return
+		}
+
+		err = reply.Unmarshal(replyB)
+
+		glog.Info("clif SayHelloCliStream result:", reply, err)
+	}()
+
+	go func() {
+		// 服务端流
+		req := &demo.Request{
+			Name: "22",
+		}
+		reply := &demo.Reply{}
+
+		reqB, _ := req.Marshal()
+
+		sreamDesc := &grpc.StreamDesc{
+			//StreamName:    "SayHelloCliStream",
+			//ClientStreams: true,
+			ServerStreams: true,
+		}
+
+		serStream, err := grpc.NewClientStream(context.Background(), sreamDesc, conn, "/demo.demo/SayHelloServStream")
+		if err != nil {
+			glog.Info("clif SayHelloServStream err:", err)
+			return
+		}
+
+		err = serStream.SendMsgForward(reqB)
+		if err != nil {
+			glog.Info("clif SayHelloServStream SendMsgForward err:", err)
+			return
+		}
+
+		err = serStream.CloseSend()
+		if err != nil {
+			glog.Info("clif SayHelloServStream CloseSend err:", err)
+			return
+		}
+
+		for {
+			replyB, err := serStream.RecvMsgForward()
+			if err == io.EOF {
+				glog.Info("clif SayHelloServStream end")
+				return
+			}
+			if err != nil {
+				glog.Info("clif SayHelloServStream err:", err)
+			}
+
+			err = reply.Unmarshal(replyB)
+
+			glog.Info("clif SayHelloServStream got:", reply)
+		}
+	}()
+
+	go func() {
+		// 双向流
+		req := &demo.Request{
+			Name: "bi",
+		}
+		reply := &demo.Reply{}
+
+		sreamDesc := &grpc.StreamDesc{
+			//StreamName:    "SayHelloCliStream",
+			ClientStreams: true,
+			ServerStreams: true,
+		}
+
+		biStream, err := grpc.NewClientStream(context.Background(), sreamDesc, conn, "/demo.demo/SayHelloBidiStream")
+		if err != nil {
+			glog.Info("clif SayHelloBidiStream err:", err)
+			return
+		}
+
+		for i := 0; i < 8; i++ {
+			req.Name = "bi " + strconv.Itoa(i)
+			reqB, _ := req.Marshal()
+			err = biStream.SendMsgForward(reqB)
+			if err != nil {
+				glog.Info("clif SayHelloBidiStream send err:", err)
+			}
+
+			replyB, err := biStream.RecvMsgForward()
+
+			if err != nil {
+				glog.Info("clif SayHelloBidiStream recv err:", err)
+			}
+			if err == io.EOF {
+				glog.Info("clif SayHelloBidiStream got end")
+				break
+			}
+			err = reply.Unmarshal(replyB)
+			glog.Info("clif SayHelloBidiStream got:", reply)
+		}
+
+		biStream.CloseSend()
+
+		for {
+			replyB, err := biStream.RecvMsgForward()
+			if err == io.EOF {
+				glog.Info("clif SayHelloBidiStream got end")
+				return
+			}
+			if err != nil {
+				glog.Info("clif SayHelloBidiStream recv err:", err)
+				return
+			}
+
+			err = reply.Unmarshal(replyB)
+
+			glog.Info("clif SayHelloBidiStream got:", reply)
+
+		}
+	}()
+
+	for {
+		time.Sleep(time.Minute)
+		return
 	}
 }
